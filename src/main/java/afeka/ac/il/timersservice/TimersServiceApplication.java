b6d0baaa6176ea09e38cce13a5e4a2692108019b
YosefOwl <93843185+YosefOwl@users.noreply.github.com>
2024-02-28 11:20:02 +0200

    first commit

---
+++ src/main/java/afeka/ac/il/timersservice/TimersServiceApplication.java
@@ -0,0 +1,13 @@
+package afeka.ac.il.timersservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimersServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimersServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/afeka/ac/il/timersservice/TimersServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package afeka.ac.il.timersservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimersServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
